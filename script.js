8828040fb529c7f218e922322e811e3358728eb3
jesusoyer <oyervidesjesus017@gmail.com>
2022-04-14 14:15:34 -0500

    added reset button to clear local storage

--- script.js
+++ script.js
@@ -1,126 +1,7 @@
 var today= moment();
-console.log(today)
-var dayOfWeek = moment().format('dddd, ' + "LL");   
-$("#currentDay").text(dayOfWeek);
 
 
-var dayTime = moment().format('LT');
-console.log(dayTime)
- 
-
-var startTime = moment(today).hour(9);//9am
-console.log(startTime)
-
-var startTime2 = moment(today).hour(10);//9am
-console.log(startTime2)
-
-var startTime3 = moment(today).hour(11);//11am
-console.log(startTime3)
-
-var startTime4 = moment(today).hour(12);//12pm
-console.log(startTime4)
-
-var startTime5 = moment(today).hour(13);//1pm
-console.log(startTime5)
-
-var startTime6 = moment(today).hour(14);//2pm
-console.log(startTime6)
-
-var startTime7 = moment(today).hour(15);//3pm
-console.log(startTime7)
-
-var startTime8 = moment(today).hour(16);//4pm
-console.log(startTime8)
-
-var startTime9 = moment(today).hour(17);//5pm
-console.log(startTime9)
-
-function colorBlocks(){
-    
-if(moment(startTime).isSame(today)){
-    $("#toDoList").css("background-color", "lightgrey")
-} else if(moment(today).isAfter(startTime)){
-    $("#toDoList").css("background-color", "red") 
-} else{
-    $("#toDoList").css("background-color", "green") 
-}
-
-
-
-if(moment(startTime2).isSame(today)){
-    $("#toDoList2").css("background-color", "lightgrey")
-} else if(moment(today).isAfter(startTime2)){
-    $("#toDoList2").css("background-color", "red") 
-} else{
-    $("#toDoList2").css("background-color", "green") 
-}
-
-
-if(moment(startTime3).isSame(today)){
-    $("#toDoList3").css("background-color", "lightgrey")
-} else if(moment(today).isAfter(startTime3)){
-    $("#toDoList3").css("background-color", "red") 
-} else{
-    $("#toDoList3").css("background-color", "green") 
-}
-
-if(moment(startTime4).isSame(today)){
-    $("#toDoList4").css("background-color", "lightgrey")
-} else if(moment(today).isAfter(startTime4)){
-    $("#toDoList4").css("background-color", "red") 
-} else{
-    $("#toDoList4").css("background-color", "green") 
-}
-
-
-if(moment(startTime5).isSame(today)){
-    $("#toDoList5").css("background-color", "lightgrey")
-} else if(moment(today).isAfter(startTime5)){
-    $("#toDoList5").css("background-color", "red") 
-} else{
-    $("#toDoList5").css("background-color", "green") 
-}
-
-
-if(moment(startTime6).isSame(today)){
-    $("#toDoList6").css("background-color", "lightgrey")
-} else if(moment(today).isAfter(startTime6)){
-    $("#toDoList6").css("background-color", "red") 
-} else{
-    $("#toDoList6").css("background-color", "green") 
-}
-
-
-if(moment(startTime7).isSame(today)){
-    $("#toDoList7").css("background-color", "lightgrey")
-} else if(moment(today).isAfter(startTime7)){
-    $("#toDoList7").css("background-color", "red") 
-} else{
-    $("#toDoList7").css("background-color", "green") 
-}
-
-
-if(moment(startTime8).isSame(today)){
-    $("#toDoList8").css("background-color", "lightgrey")
-} else if(moment(today).isAfter(startTime8)){
-    $("#toDoList8").css("background-color", "red") 
-} else{
-    $("#toDoList8").css("background-color", "green") 
-}
-
-
-if(moment(startTime9).isSame(today)){
-    $("#toDoList9").css("background-color", "lightgrey")
-} else if(moment(today).isAfter(startTime9)){
-    $("#toDoList9").css("background-color", "red") 
-} else{
-    $("#toDoList9").css("background-color", "green") 
-}
-}
-
-
-
-
+/// assigning variables to ID's from html documents
 var firstDiv = $("#toDoList")
 var secondDiv = $("#toDoList2")
 var thirdDiv = $("#toDoList3")
@@ -135,6 +16,131 @@ var ninthDiv = $("#toDoList9")
 
 
 
+//Displaying day of week and date at the top of page
+var dayOfWeek = moment().format('dddd, ' + "LL");   
+$("#currentDay").text(dayOfWeek);
+
+
+ // function houses the conditionals for th input blocks
+function colorBlocks(){
+
+// conditional for the 9am input block    
+if(moment(moment(today).hour(9)).isSame(today)){
+    $("#toDoList").css("background-color", "lightgrey")
+    $("#toDoList").css("color", "black")
+} else if(moment(today).isAfter(moment(today).hour(9))){
+    $("#toDoList").css("background-color", "red") 
+    $("#toDoList").css("color", "white")
+} else{
+    $("#toDoList").css("background-color", "green")
+    $("#toDoList").css("color", "white")
+}
+
+
+// conditional for the 10am input block
+if(moment(moment(today).hour(10)).isSame(today)){
+    $("#toDoList2").css("background-color", "lightgrey")
+    $("#toDoList2").css("color", "black")
+} else if(moment(today).isAfter(moment(today).hour(10))){
+    $("#toDoList2").css("background-color", "red") 
+    $("#toDoList2").css("color", "white")
+} else{
+    $("#toDoList2").css("background-color", "green")
+    $("#toDoList2").css("color", "white")
+}
+
+// conditional for the 11am input block
+if(moment(moment(today).hour(11)).isSame(today)){
+    $("#toDoList3").css("background-color", "lightgrey")
+    $("#toDoList3").css("color", "black")
+} else if(moment(today).isAfter(moment(today).hour(11))){
+    $("#toDoList3").css("background-color", "red")
+    $("#toDoList3").css("color", "white")
+} else{
+    $("#toDoList3").css("background-color", "green") 
+    $("#toDoList3").css("color", "white")
+}
+// conditional for the 12pm input block  
+if(moment(moment(today).hour(12)).isSame(today)){
+    $("#toDoList4").css("background-color", "lightgrey")
+    $("#toDoList4").css("color", "black")
+} else if(moment(today).isAfter(moment(today).hour(12))){
+    $("#toDoList4").css("background-color", "red") 
+    $("#toDoList4").css("color", "white")
+} else{
+    $("#toDoList4").css("background-color", "green")
+    $("#toDoList4").css("color", "white")
+}
+
+// conditional for the 1pm input block  
+if(moment(moment(today).hour(13)).isSame(today)){
+    $("#toDoList5").css("background-color", "lightgrey")
+    $("#toDoList5").css("color", "black")
+} else if(moment(today).isAfter(moment(today).hour(13))){
+    $("#toDoList5").css("background-color", "red")
+    $("#toDoList5").css("color", "white")
+} else{
+    $("#toDoList5").css("background-color", "green")
+    $("#toDoList5").css("color", "white")
+}
+
+// conditional for the 2pm input block 
+if(moment(moment(today).hour(14)).isSame(today)){
+    $("#toDoList6").css("background-color", "lightgrey")
+    $("#toDoList6").css("color", "black")
+} else if(moment(today).isAfter(moment(today).hour(14))){
+    $("#toDoList6").css("background-color", "red") 
+    $("#toDoList6").css("color", "white")
+} else{
+    $("#toDoList6").css("background-color", "green")
+    $("#toDoList6").css("color", "white")
+}
+
+// conditional for the 3pm input block 
+if(moment(moment(today).hour(15)).isSame(today)){
+    $("#toDoList7").css("background-color", "lightgrey")
+    $("#toDoList7").css("color", "black")
+} else if(moment(today).isAfter(moment(today).hour(15))){
+    $("#toDoList7").css("background-color", "red") 
+    $("#toDoList7").css("color", "white")
+} else{
+    $("#toDoList7").css("background-color", "green")
+    $("#toDoList7").css("color", "white")
+}
+
+// conditional for the 4pm input block 
+if(moment(moment(today).hour(16)).isSame(today)){
+    $("#toDoList8").css("background-color", "lightgrey")
+    $("#toDoList8").css("color", "black")
+} else if(moment(today).isAfter(moment(today).hour(16))){
+    $("#toDoList8").css("background-color", "red") 
+    $("#toDoList8").css("color", "white")
+} else{
+    $("#toDoList8").css("background-color", "green")
+    $("#toDoList8").css("color", "white")
+}
+
+// conditional for the 5pm input block 
+if(moment(moment(today).hour(17)).isSame(today)){
+    $("#toDoList9").css("background-color", "lightgrey")
+    $("#toDoList9").css("color", "black")
+} else if(moment(today).isAfter(moment(today).hour(17))){
+    $("#toDoList9").css("background-color", "red") 
+    $("#toDoList9").css("color", "white")
+} else{
+    $("#toDoList9").css("background-color", "green") 
+    $("#toDoList9").css("color", "white")
+}
+}
+
+
+
+
+
+
+
+
+// storing and getting local storage item for the first input Div
 $("#button1").click(function(event){
     event.preventDefault();
     var name = firstDiv.val();
@@ -147,6 +153,7 @@ function getValue(){
     firstDiv.val(nameFromLocalStorage)
     }
 //////////////////////////////////////////////////////////////////
+// storing and getting local storage item for the second input Div
     $("#button2").click(function(event){
         event.preventDefault();
         var name2 = secondDiv.val();
@@ -158,6 +165,7 @@ function getValue(){
         secondDiv.val(nameFromLocalStorage2)
         }
 //////////////////////////////////////////////////////////////////
+// storing and getting local storage item for the third input Div
         $("#button3").click(function(event){
             event.preventDefault();
             var name3 = thirdDiv.val();
@@ -170,6 +178,7 @@ function getValue(){
             thirdDiv.val(nameFromLocalStorage3)
             }
 ///////////////////////////////////////////////////////////////////
+// storing and getting local storage item for the fourth input Div
 $("#button4").click(function(event){
     event.preventDefault();
     var name4 = fourthDiv.val();
@@ -182,6 +191,7 @@ function getValue4(){
     fourthDiv.val(nameFromLocalStorage4)
     }
 //////////////////////////////////////////////////////////////////
+// storing and getting local storage item for the fifth input Div
 $("#button5").click(function(event){
     event.preventDefault();
     var name5 = fifthDiv.val();
@@ -194,6 +204,7 @@ function getValue5(){
     fifthDiv.val(nameFromLocalStorage5)
     }
 //////////////////////////////////////////////////////////////
+// storing and getting local storage item for the sixth input Div
 $("#button6").click(function(event){
     event.preventDefault();
     var name6 = sixthDiv.val();
@@ -206,6 +217,7 @@ function getValue6(){
     sixthDiv.val(nameFromLocalStorage6)
     }
 ////////////////////////////////////////////////////////////// 
+// storing and getting local storage item for the seventh input Div
 $("#button7").click(function(event){
     event.preventDefault();
     var name7 = seventhDiv.val();
@@ -218,6 +230,7 @@ function getValue7(){
     seventhDiv.val(nameFromLocalStorage7)
     }
 ///////////////////////////////////////////////////////////////////
+// storing and getting local storage item for the eighth input Div
 $("#button8").click(function(event){
     event.preventDefault();
     var name8 = eighthDiv.val();
@@ -229,7 +242,8 @@ function getValue8(){
     var nameFromLocalStorage8 = window.localStorage.getItem("todaysEvents8")
     eighthDiv.val(nameFromLocalStorage8)
     }
- ////////////////////////////////////////////////////////////////   
+ //////////////////////////////////////////////////////////////// 
+   // storing and getting local storage item for the ninth input Div
  $("#button9").click(function(event){
     event.preventDefault();
     var name9 = ninthDiv.val();
@@ -243,8 +257,53 @@ function getValue9(){
     }
 
 
+        // resets the entire day planner input area by clicking the reset button
+    $("#reset").click(function(){
+        
+        localStorage.removeItem("todaysEvents");
+        var removedsavedInput = window.localStorage.getItem("todaysEvents")
+        firstDiv.val(removedsavedInput)
 
-    
+        localStorage.removeItem("todaysEvents2");
+        var removedsavedInput2 = window.localStorage.getItem("todaysEvents2")
+        secondDiv.val(removedsavedInput2)
+
+        localStorage.removeItem("todaysEvents3");
+        var removedsavedInput3 = window.localStorage.getItem("todaysEvents3")
+        thirdDiv.val(removedsavedInput3)
+
+        localStorage.removeItem("todaysEvents4");
+        var removedsavedInput4 = window.localStorage.getItem("todaysEvents4")
+        fourthDiv.val(removedsavedInput4)
+
+        localStorage.removeItem("todaysEvents5");
+        var removedsavedInput5 = window.localStorage.getItem("todaysEvents5")
+        fifthDiv.val(removedsavedInput5)
+
+        localStorage.removeItem("todaysEvents6");
+        var removedsavedInput6 = window.localStorage.getItem("todaysEvents6")
+        sixthDiv.val(removedsavedInput6)
+
+        localStorage.removeItem("todaysEvents7");
+        var removedsavedInput7 = window.localStorage.getItem("todaysEvents7")
+        seventhDiv.val(removedsavedInput7)
+
+        localStorage.removeItem("todaysEvents8");
+        var removedsavedInput8 = window.localStorage.getItem("todaysEvents8")
+        eighthDiv.val(removedsavedInput8)
+
+        localStorage.removeItem("todaysEvents9");
+        var removedsavedInput9 = window.localStorage.getItem("todaysEvents9")
+        ninthDiv.val(removedsavedInput9)
+        
+
+
+    });
+
+
+
+
+    // calls local storage functions to display on page
     getValue();
     getValue2();
     getValue3();
@@ -255,5 +314,7 @@ function getValue9(){
     getValue8();
     getValue9();
 
+    //calls on the color block function to update every 10000 of a second
     setInterval(colorBlocks,10000)
+    // calls the function that stores conditionals to be displayed on the page
     colorBlocks();
